d3f305d651f1b994cf24068c03aea495160786b0
jas <jas@jass-mbp-2.lan>
2021-11-23 03:17:45 -0600

    corrects Resume title in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './'
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Resume />
+    <Resume/>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
