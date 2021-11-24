4c60b07a40e89ac47704ca36df3cb7c9a3bad418
Stephen Johnson <stephenjayyy@gmail.com>
2021-11-24 10:54:50 -0600

    Corrects folder name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Resume from "./Resume/resume";
+import Resume from "./Components/resume";
 
 ReactDOM.render(
   <React.StrictMode>
