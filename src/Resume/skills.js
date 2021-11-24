cb95c0cc60b23d360098c54389e8d201a627330c
Stephen Johnson <stephenjayyy@gmail.com>
2021-11-23 18:33:34 -0600

    Adds animation

--- src/Resume/skills.js
+++ src/Resume/skills.js
@@ -7,11 +7,16 @@ function Skills() {
         <h2>Skills</h2>
         <p>
           Operating Systems: Microsoft Windows (7, 8, 10), Microsoft Server
-          (2003, 2008, 2012), Mac OS, Android, IOS, Ubuntu Linux Hardware:
-          Servers, Workstations, Cisco switches, SonicWALL firewalls,
-          pfSense/FreeBSD based firewalls Software: Microsoft Office, Office 365
-          Parallels Desktop , VMware, Hyper-V, Active Directory, Exchange, SQL,
-          SMTK , Microsoft Azure , GDCO
+          (2003, 2008, 2012), Mac OS, Android, IOS, Ubuntu Linux
+        </p>
+        <p>
+          Hardware: Servers, Workstations, Cisco switches, SonicWALL firewalls,
+          pfSense/FreeBSD based firewalls
+        </p>
+        <p>
+          Software: Microsoft Office, Office 365 Parallels Desktop , VMware,
+          Hyper-V, Active Directory, Exchange, SQL, SMTK , Microsoft Azure ,
+          GDCO
         </p>
       </div>
     </>
